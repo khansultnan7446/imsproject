bdd0e7f19763a18f37baadd143e1e9f10f3b291e
khansultnan7446 <khansultan7446@gmail.com>
2024-11-25 14:16:44 +0530

    page not found implemented

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './component/login/login.component';
+import { DashboardComponent } from './component/dashboard/dashboard.component';
+import { PagenotfoundComponent } from './component/pagenotfound/pagenotfound.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'login' , component:LoginComponent},
+  {path:'dashboard' , component:DashboardComponent},
+  {path:'' , component:LoginComponent},
+  {path:'**' , component:PagenotfoundComponent}
+  
+  
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
